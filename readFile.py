ffb322216adb7f93f676cac0c5f4ab7e59fed4c5
tamaksHarsh <47510647+tamaksHarsh@users.noreply.github.com>
2019-12-10 21:18:15 -0500

    Update readFile.py

--- readFile.py
+++ readFile.py
@@ -3,27 +3,51 @@ import scipy as sp
 import numpy as np
 import time
 import pandas as pd
-"""
-df = pd.read_csv("data.tsv",delimiter="\t",usecols=(0,1,2,5,7),nrows=100)
-data = df.values
 
-print("Printing Attributes: \n")
-print(df['attributes'].values)
 
-print("Printing Titles: \n")
-print(df['title'].values)
+def readFile(filename,nRows):
+    df = pd.read_csv(filename,delimiter="\t",usecols=(0,1,2,5,7),nrows=nRows)
+    data = df.values
 
-print("Printing Subtitles: \n")
-print(df['subtitle'].values)
+    print(data)
+    '''
+    print("Printing Attributes: \n")
+    print(df['attributes'].values)
 
-print("Printing Categories: \n")
-#print(df['category'].values)
-print(df.category.unique())
-"""
-#data = np.genfromtxt("mlchallenge_set_validation.tsv",delimiter="\t",usecols=(0,1,2,5,7),max_rows=1000)
-data = np.genfromtxt("mlchallenge_set_validation.tsv",delimiter="\t")
-print(data[:,-1])
-print(len(np.unique(data[:,:1])))
+    print("Printing Titles: \n")
+    print(df['title'].values)
+
+    print("Printing Subtitles: \n")
+    print(df['subtitle'].values)
+
+    print("Printing Categories: \n")
+    #print(df['category'].values)
+    print(df.category.unique())
+    '''
+    return data
+
+def readValidation(filename):
+    #data = np.genfromtxt("mlchallenge_set_validation.tsv",delimiter="\t",usecols=(0,1,2,5,7),max_rows=1000)
+    data = np.genfromtxt("mlchallenge_set_validation.tsv",delimiter="\t")
+    print(data[:,-1])
+    print(len(np.unique(data[:,:1])))
+    return data
+
+def processAttributes(attribute):
+
+
+
+
+# Start of Program
+nRows = 5
+data = readFile("data.csv",nRows)
+
+attribute = df['attributes'].values
+
+
+
+
+
 
 #print(data[1:6])
 #print(len(df))
