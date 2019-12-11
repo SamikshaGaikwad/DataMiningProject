d1c5b40a847ca16668f868617f70bfc7825152d9
tamaksHarsh <47510647+tamaksHarsh@users.noreply.github.com>
2019-12-10 22:51:00 -0500

    Adding the feature of processing attribute
    Generates a master list of all attributes in the file

--- readFile.py
+++ readFile.py
@@ -3,12 +3,24 @@ import scipy as sp
 import numpy as np
 import time
 import pandas as pd
+import re
+
+# Python code to merge dict using a single
+# expression
+def mergeDict(dict1, dict2):
+    return(dict2.update(dict1))
+
+def findKeys(dict):
+    keylist = []
+    for thiskey in dict.keys():
+        keylist.append(thiskey)
+    return keylist
 
 def readFile(filename,nRows):
     df = pd.read_csv(filename,delimiter="\t",usecols=(0,1,2,5,7),nrows=nRows)
     data = df.values
 
-    print(data)
+    #print(data)
     '''
     print("Printing Attributes: \n")
     print(df['attributes'].values)
@@ -32,21 +44,47 @@ def readValidation(filename):
     print(len(np.unique(data[:,:1])))
     return data
 
-#def processAttributes(attribute):
+def createMasterAttributeList(attribute):
+    attList = []
+    for thisattri in attribute[1:]:
+        #print(thisattri)
+        attList.extend(findKeys(processAttributeStr(thisattri)))
+    return attList
 
+def printMasterAttributeList(attList,fname):
+    np.savetxt(fname, attList,fmt='%s', delimiter='\n')
+
+def processAttributeStr(thisattri):
+    attDict = dict()
+    thisstr = thisattri.strip('(').strip(')')
+    thisstr = re.split(":+",thisstr)
+    #print(thisstr)
+    for idx in range(len(thisstr)-1):
+        attriTitle = re.split(',',thisstr[idx])[-1]
+        attriVal = re.split(',',thisstr[idx+1])[0:-1]
+        attDict[attriTitle] = attriVal
+
+        #print(attDict)
+    #print(attDict.keys())
+    #print('')
+    return attDict
 
 
 
 # Start of Program
-nRows = 5
+nRows = 1000
 df = readFile("data.tsv",nRows)
 
 attribute = df['attributes'].values
 
+a = createMasterAttributeList(attribute)
 
+printMasterAttributeList(np.sort(np.unique(a)),'attributeList.csv')
+print(np.unique(a))
 
 
 
 
 #print(data[1:6])
 #print(len(df))
+
