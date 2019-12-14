88a07614696334df9d89aae4ef54911ab780481b
tamaksHarsh <47510647+tamaksHarsh@users.noreply.github.com>
2019-12-14 01:46:23 -0500

    More pre-processing function to input data file

--- readFile.py
+++ readFile.py
@@ -5,10 +5,15 @@ import time
 import pandas as pd
 import re
 import numpy as np
-import sklearn.cluster
-import distance
 import mmap
-
+import sys
+import nltk
+stemmer = nltk.stem.porter.PorterStemmer()
+#from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.datasets import load_files
+#nltk.download('stopwords')
+#import pickle
+#from nltk.corpus import stopwords
 
 from processAttribute import *
 
@@ -28,22 +33,6 @@ def countFileLines(filename):
 def readFile(filename,nRows):
     df = pd.read_csv(filename,delimiter="\t",usecols=(0,1,2,5,7),nrows=nRows)
     data = df.values
-
-    #print(data)
-    '''
-    print("Printing Attributes: \n")
-    print(df['attributes'].values)
-
-    print("Printing Titles: \n")
-    print(df['title'].values)
-
-    print("Printing Subtitles: \n")
-    print(df['subtitle'].values)
-
-    print("Printing Categories: \n")
-    #print(df['category'].values)
-    print(df.category.unique())
-    '''
     return df
 
 def readValidation(filename):
@@ -53,25 +42,31 @@ def readValidation(filename):
     print(len(np.unique(data[:,:1])))
     return data
 
-# FAILED: Tries to cluster list of attributes into clusters
-def clusterAttributeNames(attributes):
-    attributes = np.asarray(attributes)
-    lev_similarity = -1*np.array([[distance.levenshtein(w1,w2) for w1 in attributes] for w2 in attributes])
-    affprop = sklearn.cluster.AffinityPropagation(affinity="precomputed", damping=0.5)
-    affprop.fit(lev_similarity)
-    for cluster_id in np.unique(affprop.labels_):
-        exemplar = attributes[affprop.cluster_centers_indices_[cluster_id]]
-        cluster = np.unique(attributes[np.nonzero(affprop.labels_==cluster_id)])
-        cluster_str = ", ".join(cluster)
-        print(" - *%s:* %s" % (exemplar, cluster_str))
+def preProcess(df):
+    for j in df.columns:
+        if j in ['title', 'attributes']:
+            thisDf = df[j].values
+            for i in range(len(thisDf)):
+                # Remove all special characters other than : and ,
+                thisDf[i] = re.sub('[^,:,a-zA-Z0-9 \n\.]', ' ', thisDf[i])
 
+                # remove all single characters
+                thisDf[i] = re.sub(r'\s+[a-zA-Z]\s+', '', thisDf[i])
+
+                # Convert to lower case and strip extra spaces
+                thisDf[i] = thisDf[i].lower().strip()
+
+                # Use nltk stemmer to create stemmed words
+                thisDf[i]  = [stemmer.stem(word) for word in thisDf[i]]
+                thisDf[i] =''.join(thisDf[i])
+            # Assign the value back into dataframe
+            df[j].value = thisDf
+    return df
 
 # Start of Program
 nRows = 1002276 #maxRows = 1002276
 #cutOff = 5 # Cutoff weights -Ignores attributes repeated less than cutoff times
 #attriWordsToIgnore = ['condition','price','shipping','return','location']
-
-
 
 filename = "data.tsv"
 start_time = time.time()
@@ -80,14 +75,24 @@ start_time = time.time()
 #end_time = time.time() - start_time
 
 df = readFile(filename,nRows)
+#print(df.columns)
+df = preProcess(df)
 
-# Find all attributes and convert to lower case
+
+#attribute = [x.lower().strip() for x in attribute]
+#print( df['title'].values)
 attribute = df['attributes'].values
-attribute = [x.lower().strip() for x in attribute]
-
+np.set_printoptions(threshold=sys.maxsize)
+#print(np.array(df))
+'''
+tfidfconverter = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7)
+X = tfidfconverter.fit_transform(attribute).toarray()
+np.set_printoptions(threshold=sys.maxsize)
+print(X)
+'''
 filteredAttribute = createAttributeList(attribute)
 
-#clusterAttributeNames(filteredAttribute)
+
 
 #print(np.unique(a))
 print("Time Taken: " + str(time.time() - start_time))
